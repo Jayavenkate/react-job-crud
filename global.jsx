1aee6b90ac7a6104ddd8b0cb1dea2371de0b5da7
Jayavenkate <jayajaikithuja@gmail.com>
2024-03-10 22:08:45 +0300

    ui

--- global.jsx
+++ global.jsx
@@ -1,3 +1,3 @@
-
-export const API = "https://node-job-crud.vercel.app";
 // export const API = "http://localhost:4003";
+// eslint-disable-next-line react-refresh/only-export-components
+export const API = "https://node-job-crud.onrender.com"
